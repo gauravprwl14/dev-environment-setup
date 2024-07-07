86fd7bd1c7117e00e91d0a81dc7b4fdf7c5b340f
Gaurav Porwal <gauravprwl14@gmail.com>
2024-07-07 09:13:29 +0530

    added the basic setup script, and adding the home brew script to install and update the home brew

---
+++ setup/install_homebrew.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null; then
+    echo "Homebrew is not installed. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed. Updating Homebrew..."
+    brew update
+fi
+
---
+++ setup/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ensure the scipt is run from the directory containing the other script
+cd "$(dirname "$0")"
+
+
+# Install Homebrew
+./install_homebrew.sh
+
+
+echo " All installations complete. Exiting..."
+
+
